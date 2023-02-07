e82d6ad72f91c5ced9a5ff632428dd9ea5097e1e
Maheswari Manoharan <maheswari.manoharan@Maheswaris-MacBook-Pro.local>
2023-02-07 16:59:58 -0500

    Test tag

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import string
 def main():
     length = 5 #if os.environ['INPUT_WORD_LENGTH']
     letters = string.ascii_lowercase
-    # word =  ''.join(random.choice(letters) for i in range(length))
-    # print(f"::set-output name=word::{word}")
     if "GITHUB_OUTPUT" in os.environ:
         with open(os.environ["GITHUB_OUTPUT"], "w") as f:
             print("{}={}".format("random_word", ''.join(random.choice(letters) for i in range(length))), file=f)
